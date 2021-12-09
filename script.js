0ed8a56cdc381086e89486f3a688a7797f661d2e
tpvale <tpv.tiago@gmail.com>
2021-12-08 22:40:32 -0300

    inclusão das imagens e videos na aba login

---
+++ script.js
@@ -0,0 +1,7 @@
+// LOGIN
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
